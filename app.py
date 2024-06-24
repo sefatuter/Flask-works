e8071e5ea9e0555b09d22fd17b17a20e6bb722ba
sefatuter <sefa2233_18@hotmail.com>
2024-06-25 00:55:28 +0300

    Login and Logout parts added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm # pip install flask_wtf
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user # pip install flask_login
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -45,6 +46,65 @@ migrate = Migrate(app, db)
 # $ flask db stamp head
 # $ flask db migrate
 # $ flask db upgrade
+
+
+# Flask Login Stuff
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+# Create Login Form
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Create Login Page
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first() # First searchs for username if it exist. ( username is unique )
+        if user:
+            # Check the hash
+            if check_password_hash(user.password_hash, form.password.data):
+                login_user(user)
+                flash("Login Successfull", "success")
+                return redirect(url_for('dashboard'))     
+            else:
+                flash("Wrong Password Try Again", "danger")
+        else:
+            flash("This user does not exist!")
+            
+    return render_template('login.html', form=form)
+
+
+
+# Create Logout Page
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    flash("You Have Been Logged Out!!")
+    return redirect(url_for('login'))
+
+
+
+# Create Dashboard Page
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 # Create a Blog Post Model
 class Posts(db.Model):
@@ -179,9 +239,14 @@ def get_current_date():
     # return {"Date": datetime.date.today()}
 
 
+
+
+
+
 # Create Model
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True) # flask db migrate -m "added username", flask db  upgrade
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
@@ -244,6 +309,7 @@ class NameForm(FlaskForm):
 
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favorite_color = StringField("Favorite Color")
     password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo('password_hash2', message="Passwords must match!")])
@@ -298,7 +364,8 @@ def update(id):
             flash("Error! ...try again")
             return render_template("update.html", 
                 form=form,
-                name_to_update=name_to_update)
+                name_to_update=name_to_update,
+                id=id)
     else:
         return render_template("update.html", 
                 form=form,
@@ -315,8 +382,9 @@ def add_user():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
             # Hash the password
-            hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
-            user = Users(name=form.name.data,
+            hashed_pw = generate_password_hash(form.password_hash.data, "pbkdf2:sha256")
+            user = Users(username = form.username.data,
+                         name=form.name.data,
                          email=form.email.data,
                          favorite_color=form.favorite_color.data,
                          password_hash=hashed_pw) # instead of password_hash=form.password_hash.data
@@ -324,6 +392,7 @@ def add_user():
             db.session.commit()
         name = form.name.data
         form.name.data = ''
+        form.username.data = ''
         form.email.data = ''
         form.favorite_color.data = ''
         form.password_hash.data = ''
