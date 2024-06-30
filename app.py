cec5eeb49e099e9e83320271e8e8afad64a6ae5b
sefatuter <sefa2233_18@hotmail.com>
2024-07-01 00:37:15 +0300

    null profile pic

--- app.py
+++ app.py
@@ -147,36 +147,43 @@ def dashboard():
         name_to_update.favorite_color = request.form['favorite_color']
         name_to_update.about_author = request.form['about_author']
         name_to_update.username = request.form['username']
-        name_to_update.profile_pic = request.files['profile_pic']
         
+        # Check for profile pic
+        
+        if request.files['profile_pic']:
+            name_to_update.profile_pic = request.files['profile_pic']
+                    
+            #Grab image name
+            pic_filename = secure_filename(name_to_update.profile_pic.filename)
+            
+            # Set UUID
+            pic_name = str(uuid.uuid1()) + "_" + pic_filename # Creating Unique filename
+            
+            # Save The Image
+            saver = request.files['profile_pic']
+            
+            # Change it to a string to save to database
+            name_to_update.profile_pic = pic_name
+            try:
+                db.session.commit()
+                saver.save(os.path.join(app.config['UPLOAD_FOLDER'], pic_name))    
+                flash("User Updated Successfully!")
 
-        
-        #Grab image name
-        pic_filename = secure_filename(name_to_update.profile_pic.filename)
-        
-        # Set UUID
-        pic_name = str(uuid.uuid1()) + "_" + pic_filename # Creating Unique filename
-        
-        # Save The Image
-        saver = request.files['profile_pic']
-        
-        # Change it to a string to save to database
-        name_to_update.profile_pic = pic_name
-        try:
+                return render_template("dashboard.html", 
+                    form=form,
+                    name_to_update=name_to_update)
+            except:
+                db.session.commit()
+                flash("Error! ...try again")
+                return render_template("dashboard.html", 
+                    form=form,
+                    name_to_update=name_to_update,
+                    id=id)
+        else:
             db.session.commit()
-            saver.save(os.path.join(app.config['UPLOAD_FOLDER'], pic_name))    
             flash("User Updated Successfully!")
-
-            return render_template("dashboard.html", 
-                form=form,
-                name_to_update=name_to_update)
-        except:
-            db.session.commit()
-            flash("Error! ...try again")
-            return render_template("dashboard.html", 
-                form=form,
-                name_to_update=name_to_update,
-                id=id)
+            return render_template("dashboard.html", form=form, name_to_update=name_to_update)
+            
     else:
         return render_template("dashboard.html", 
                 form=form,
